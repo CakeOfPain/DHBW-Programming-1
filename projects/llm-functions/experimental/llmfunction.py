36d859e37c2d44864ef0966277ac0cab14bd94f9
Leo Englert <Leo.Englert@outlook.de>
2023-12-19 09:31:30 +0100

    Added llmfunctions as example in experimental

--- projects/llm-functions/experimental/llmfunction.py
+++ projects/llm-functions/experimental/llmfunction.py
@@ -1,22 +1,54 @@
+import requests
+import random
+import json
+
+ollama_url = lambda path: "http://127.0.0.1:11434" + path
 
 class LlmFunction(object):
-    def __init__(self, label: str, description: str, stops: list[str] = [], temperature = 0.0, seed = 0):
+    def __init__(self, model: str, label: str, description: str, stops: list[str] = [], temperature = 0.0, seed = random.randint(0, 2**63)):
+        self.model = model
         self.prompt = ""
         self.temperature = temperature
         self.seed  = seed
         self.description = description
         self.label = label
         self.stops = stops
+        self.result = ""
+
     def write(self, message) -> None:
         self.prompt += message
     def writeLine(self, message) -> None:
         self.prompt += message + "\n"
     def run(self):
-        pass
+        data = json.dumps({
+            "model": self.model,
+            "template": self.prompt,
+            "stream": False,
+            "options": {
+                "stop": self.stops,
+                "temperature": self.temperature,
+                "seed": self.seed,
+            }
+        })
+        response = requests.post(ollama_url("/api/generate"), data=data)
+        return response.json()['response']
 
 class EmailWriter(LlmFunction):
-    def __init__(self, subject: str, sender: str, receiver: str):
+    def __init__(self, subject: str, sender: str, receiver: str, context: str = None):
         super().__init__(
-            "EmailWriter",
-            "Writes an email from an given subject, sender and receiver"
+            model="mistral:latest",
+            label="EmailWriter",
+            description="Writes an email from an given subject, sender and receiver",
+            stops=["###"]
         )
+
+        if context != None:
+            self.writeLine("### context:")
+            self.writeLine(context)
+        self.writeLine("### subject:")
+        self.writeLine(subject)
+        self.writeLine("### sender:")
+        self.writeLine(sender)
+        self.writeLine("### receiver:")
+        self.writeLine(receiver)
+        self.writeLine("### content:")
