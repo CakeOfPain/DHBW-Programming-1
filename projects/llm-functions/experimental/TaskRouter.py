4631087ba2dbc07b66899144b1e3242e9101c5ea
Leo Englert <Leo.Englert@outlook.de>
2023-12-21 23:44:19 +0100

    Added more llm-functions

--- projects/llm-functions/experimental/TaskRouter.py
+++ projects/llm-functions/experimental/TaskRouter.py
@@ -20,7 +20,7 @@ class TaskRouter(llmfunction.LlmFunction):
         self.writeLine("### PROGRAMS:")
         for function in functions:
             self.writeLine(f"- {function.label} -> {function.description}")
-        self.writeLine("- OTHERS-> if the message is NONESENSE or other programms are not an option")
+        self.writeLine("- OTHERS -> if the message is NONESENSE or other programms are not an option")
         if reasoning:
             self.writeLine("### REASONING:")
             self.autowrite()
@@ -43,15 +43,64 @@ class AppointmentMaker(llmfunction.LlmFunction):
         self.writeLine(request)
         self.writeLine("### appointment maker:")
 
-request = input("your request: ")
-appointmentMaker = AppointmentMaker(request=request)
-taskRouter = TaskRouter(request=request, functions=[appointmentMaker], reasoning=False)
+class QASession(llmfunction.LlmFunction):
+    def __init__(self,
+            question: str,
+            facts: str,
+            topic: str | None = None
+    ):
+        super().__init__(
+            model="mistral:latest",
+            label="Q&A-Session",
+            description="Use this for Questions",
+            stops=["###"]
+        )
 
-route = taskRouter.run()
-print(taskRouter.prompt)
+        self.writeLine("### System:")
+        self.writeLine("This is an Q&A Session")
+        self.writeLine(f"### Facts{(' about '+topic) if topic != None else ''}:")
+        self.writeLine(facts)
+        self.writeLine("### Question:")
+        self.writeLine(question)
+        self.writeLine("### Answer:")
+class FactsExtractor(llmfunction.LlmFunction):
+    def __init__(self, text):
+        super().__init__(
+            model="mistral:latest",
+            label="Facts-Extractor",
+            description="Use this for extracting facts from a text"
+        )
 
-if route in appointmentMaker.label:
-    appointment = appointmentMaker.run()
-    print(appointment)
-else:
-    print("Sorry, I can't help you with that at the moment!")
+        self.writeLine("### System:")
+        self.writeLine("This is an facts extractor, which extracts facts from a given text, and summarizes them in a list")
+        self.writeLine("### Text:")
+        self.writeLine(text)
+        self.writeLine("### Facts:")
+
+
+text = """
+# Bechtle AG
+With more than 85 IT system houses and IT trading companies in 14 European countries, we are close to our customers. The combination of direct sales of IT products with comprehensive system house services makes Bechtle a future-oriented IT partner for SMEs, corporations and public sector clients. Regionally on site, in Europe and worldwide through IT alliance partners on all continents.
+
+As a service provider with strong implementation capabilities for future-proof IT architectures, traditional IT infrastructure is just as important to us as the current topics of digitalization, cloud, modern workplace, security and IT as a service. Around 40,000 hardware and software products are available via online stores, on customized e-procurement platforms and via telesales. Experts in 94 Competence Centers across the Group also deal with a wide range of IT topics requiring intensive consulting.
+
+A comprehensive IT life cycle also means that we use professional IT remarketing to ensure the economic reuse of used IT. And when it comes to intelligent financing services, we are on hand with Bechtle Financial Services AG.
+"""
+
+factsExtractor = FactsExtractor(text=text)
+facts = factsExtractor.run()
+facts += "\n+++ You are an ai working for Bechtle. +++"
+while True:
+    request = input("your request: ")
+    appointmentMaker = AppointmentMaker(request=request)
+    qAndASession = QASession(question=request, facts=facts, topic="Bechtle")
+    taskRouter = TaskRouter(request=request, functions=[appointmentMaker, qAndASession], reasoning=True)
+
+    route = taskRouter.run()
+
+    if route in appointmentMaker.label:
+        print(appointmentMaker.run())
+    elif route in qAndASession.label:
+        print(qAndASession.run())
+    else:
+        print("Sorry, I can't help you with that at the moment!")
