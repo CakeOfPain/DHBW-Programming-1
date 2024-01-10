18a9cc28162f33124adbf1c5ab9369d9b8e46c83
Leo Englert <Leo.Englert@outlook.de>
2024-01-10 11:01:01 -0400

    Renamed KnowledgeGraph Retriever to Builder

--- projects/llm-functions/experimental/KnowledgeGraph.py
+++ projects/llm-functions/experimental/KnowledgeGraph.py
@@ -5,11 +5,11 @@ import llmfunction
 # by an knowledge graph                       #
 ###############################################
 
-class KnowledgeGraphRecaller(llmfunction.LlmFunction):
+class KnowledgeGraphBuilder(llmfunction.LlmFunction):
     def __init__(self, text: str):
         super().__init__(
             model='mistral:latest',
-            label=KnowledgeGraphRecaller.__name__,
+            label=KnowledgeGraphBuilder.__name__,
             description='Use this for retrieving data based on a knowledge graph provided',
             stops=['###']
         )
@@ -21,7 +21,7 @@ class KnowledgeGraphRecaller(llmfunction.LlmFunction):
         self.writeLine('### Knowledge-Graph:')
 
 def main():
-    knowledgeGraphRecaller = KnowledgeGraphRecaller(
+    knowledgeGraphRecaller = KnowledgeGraphBuilder(
         text='The giant panda (Ailuropoda melanoleuca), sometimes called a panda bear or simply panda, is a bear species endemic to China.[4] It is characterised by its bold black-and-white coat and rotund body. The name "giant panda" is sometimes used to distinguish it from the red panda, a neighboring musteloid. Though it belongs to the order Carnivora, the giant panda is a folivore, with bamboo shoots and leaves making up more than 99% of its diet.'
     )
     print(knowledgeGraphRecaller.prompt)
